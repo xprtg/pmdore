75bee2bf7d45461fb9f5d8a101baa38015ddc432
xprtg <benjaminvega1524@gmail.com>
2025-01-02 18:37:06 -0300

    change content

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import UpdateElectron from '@/components/update'
 import logoVite from './assets/logo-vite.svg'
 import logoElectron from './assets/logo-electron.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
   return (
     <div className='App'>
       <div className='logo-box'>
@@ -14,23 +11,7 @@ function App() {
           <img src={logoElectron} className='logo electron' alt='Electron + Vite logo' />
         </a>
       </div>
-      <h1>Electron + Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Electron + Vite logo to learn more
-      </p>
-      <div className='flex-center'>
-        Place static files into the<code>/public</code> folder <img style={{ width: '5em' }} src='./node.svg' alt='Node logo' />
-      </div>
-
-      <UpdateElectron />
+      <h1>mafecita</h1>
     </div>
   )
 }
